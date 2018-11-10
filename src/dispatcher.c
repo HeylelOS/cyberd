9d68413503e08779caabd1ecfab969f1db8a3cfb
Valentin Debon <valentin@debon.net>
2018-11-10 14:20:17 +0100

    Begun dispatcher

--- src/dispatcher.c
+++ src/dispatcher.c
@@ -1,17 +1,17 @@
 #include "dispatcher.h"
+#include "log.h"
 #include "tree.h"
+#include "fde.h"
+#include "config.h"
 
 #include <stdlib.h>
-
-struct fd_element {
-	int fd;
-};
+#include <strings.h> /* for FD_COPY, ... on Darwin */
+#include <sys/socket.h> /* shutdown() */
 
 static struct {
 	struct {
-		fd_set set;
+		fd_set activeset;
 		fd_set readset;
-		fd_set writeset;
 	} *sets;
 
 	struct tree fds;
@@ -19,17 +19,71 @@ static struct {
 
 static hash_t
 dispatcher_hash_fd(const tree_element_t *element) {
-	const struct fd_element *fde
-		= (const struct fd_element *)element;
+	const struct fd_element *fde = element;
 
 	return fde->fd;
+}
+
+static void
+dispatcher_insert(struct fd_element *fde) {
+	struct tree_node *node
+		= tree_node_create(fde);
+
+	FD_SET(fde->fd, &dispatcher.sets->activeset);
+	tree_insert(&dispatcher.fds, node);
+}
+
+static void
+dispatcher_remove(struct fd_element *fde) {
+	struct tree_node *node
+		= tree_remove(&dispatcher.fds, fde->fd);
+
+	FD_CLR(fde->fd, &dispatcher.sets->activeset);
+	tree_node_destroy(node);
+}
+
+static inline struct fd_element *
+dispatcher_find(int fd) {
+
+	return tree_find(&dispatcher.fds, fd);
+}
+
+static void
+dispatcher_preorder_cleanup(struct tree_node *node) {
+
+	if(node != NULL) {
+		struct fd_element *fde = node->element;
+
+		fde_destroy(fde);
+
+		dispatcher_preorder_cleanup(node->left);
+		dispatcher_preorder_cleanup(node->right);
+	}
+}
+
+static void
+dispatcher_deinit(void) {
+
+	dispatcher_preorder_cleanup(dispatcher.fds.root);
 }
 
 void
 dispatcher_init(void) {
 
 	dispatcher.sets = calloc(1, sizeof(dispatcher.sets));
+
 	tree_init(&dispatcher.fds, dispatcher_hash_fd);
+
+	struct fd_element *acceptor
+		= fde_create_acceptor(DISPATCHER_IPC_SOCKNAME);
+
+	if(acceptor != NULL) {
+		dispatcher_insert(acceptor);
+
+		/* Cleanup needed because we must unlink acceptors */
+		atexit(dispatcher_deinit);
+	}
+
 }
 
 int
@@ -42,8 +96,7 @@ dispatcher_lastfd(void) {
 	}
 
 	if(current != NULL) {
-		const struct fd_element *fde
-			= (const struct fd_element *)current->element;
+		const struct fd_element *fde = current->element;
 
 		lastfd = fde->fd;
 	}
@@ -51,19 +104,51 @@ dispatcher_lastfd(void) {
 	return lastfd;
 }
 
-struct fd_set *
+fd_set *
 dispatcher_readset(void) {
 
 	return &dispatcher.sets->readset;
 }
 
-struct fd_set *
-dispatcher_writeset(void) {
+static void
+dispatcher_handle_acceptor(struct fd_element *fde) {
 
-	return &dispatcher.sets->writeset;
+	log_print("Can read acceptor %d\n", fde->fd);
+}
+
+static void
+dispatcher_handle_connection(struct fd_element *fde) {
+
+	log_print("Can read connection %d\n", fde->fd);
+
+	shutdown(fde->fd, SHUT_RDWR);
+	dispatcher_remove(fde);
 }
 
 void
-dispatcher_handle(int fds) {
+dispatcher_handle(unsigned int fds) {
+	fd_set *readset = &dispatcher.sets->readset;
+
+	for(unsigned int fd = 0;
+		fd < fds;
+		fd += 1) {
+
+		if(FD_ISSET(fd, readset)) {
+			struct fd_element *fde = dispatcher_find(fd);
+
+			switch(fde->type) {
+			case FD_TYPE_ACCEPTOR:
+				dispatcher_handle_acceptor(fde);
+				break;
+			case FD_TYPE_CONNECTION:
+				dispatcher_handle_connection(fde);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	FD_COPY(&dispatcher.sets->activeset, readset);
 }
 
---
+++ src/fde.c
@@ -0,0 +1,94 @@
+#include "fde.h"
+#include "log.h"
+#include "config.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SOCKADDR_UN_MAXLEN sizeof(((struct sockaddr_un *)NULL)->sun_path)
+
+struct fd_element *
+fde_create_acceptor(const char *path) {
+	struct fd_element *fde = NULL;
+	int fd = socket(PF_LOCAL, SOCK_STREAM, 0);
+
+	if(fd != -1) {
+		struct sockaddr_un addr;
+		char * const end = stpncpy(addr.sun_path, path, SOCKADDR_UN_MAXLEN);
+
+		/* Less than or EQUAL because the maximum is
+		authorized, even if no NUL char written */
+		if(end <= addr.sun_path + SOCKADDR_UN_MAXLEN) {
+			addr.sun_len = end - addr.sun_path;
+			addr.sun_family = AF_UNIX;
+
+			if(bind(fd, (const struct sockaddr *)&addr, sizeof(addr)) == 0) {
+				if(listen(fd, DISPATCHER_CONNECTIONS_LIMIT) == 0) {
+					fde = malloc(sizeof(*fde));
+
+					fde->type = FD_TYPE_ACCEPTOR;
+					fde->fd = fd;
+				} else {
+					log_error("[cyberd fde_create_acceptor listen]");
+				}
+			} else {
+				log_error("[cyberd fde_create_acceptor bind]");
+			}
+		} else {
+			log_print("[cyberd fde_create_acceptor]: error: path overflow\n");
+		}
+	} else {
+		log_error("[cyberd fde_create_acceptor socket]");
+	}
+
+	return fde;
+}
+
+struct fd_element *
+fde_create_connection(const struct fd_element *acceptor) {
+	struct fd_element *fde = NULL;
+	struct sockaddr_un addr;
+	socklen_t len;
+	int fd = accept(acceptor->fd, (struct sockaddr *)&addr, &len);
+
+	if(fd != -1) {
+		fde = malloc(sizeof(*fde));
+
+		fde->type = FD_TYPE_CONNECTION;
+		fde->fd = fd;
+	}
+
+	return fde;
+}
+
+void
+fde_destroy(struct fd_element *fde) {
+
+	if(fde->type == FD_TYPE_ACCEPTOR) {
+		char *path;
+		struct sockaddr_un addr;
+		socklen_t len = sizeof(addr);
+
+		getsockname(fde->fd,
+			(struct sockaddr *)&addr,
+			&len);
+
+		/* Knowing we accept SOCKADDR_UN_MAXLEN size paths */
+		if(addr.sun_len == SOCKADDR_UN_MAXLEN) {
+			path = alloca(addr.sun_len + 1);
+			strncpy(path, addr.sun_path, addr.sun_len);
+		} else {
+			path = addr.sun_path;
+			path[addr.sun_len] = '\0';
+		}
+
+		unlink(path);
+	}
+
+	close(fde->fd);
+	free(fde);
+}
+
