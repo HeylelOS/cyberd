029d93dff2cb74a543eaa3bd105492958c9a308f
Valentin Debon <valentin.debon@heylelos.org>
2019-10-20 16:39:48 +0200

    Removed shell expansion concerning daemons' arguments list

--- src/cyberd/daemon_conf.h
+++ src/cyberd/daemon_conf.h
@@ -9,7 +9,7 @@
 struct daemon_conf {
 	char *path; /**< Path of the executable file */
 	char **arguments; /**< Command line arguments, including process name */
-	char **environment; /**< Command line arguments, including process name */
+	char **environment; /**< Command line environment variables */
 	char *wd; /**< Working directory of the process */
 
 	int sigend; /**< Signal used to stop the process, default SIGTERM */
